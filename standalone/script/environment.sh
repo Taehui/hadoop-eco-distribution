ed89d8d33b0243ecbd7fa8e3e6e4085be3b0ba11
Taehui <hongtebari@gmail.com>
2022-08-04 06:09:25 +0900

    Add apply environment configuration

--- standalone/script/environment.sh
+++ standalone/script/environment.sh
@@ -2,7 +2,7 @@
 
 # /etc/environment.sh
 sudo cp ~/hadoop-eco-distribution/standalone/configuration/etc/environment /etc
-
+source /etc/environment
 
 # JAVA
 sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
