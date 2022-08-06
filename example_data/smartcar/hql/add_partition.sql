6095131e8ca4a4e4eb2a624e90a007e5f0cf03f1
Taehui Hong <hongtebari@gmail.com>
2022-08-06 13:21:28 +0900

    Remove exampels data

--- example_data/smartcar/hql/add_partition.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE smartcar.smartcar_status_info ADD PARTITION(dt='20220801');
-ALTER TABLE smartcar.smartcar_status_info ADD PARTITION(dt='20220802');
-ALTER TABLE smartcar.smartcar_status_info ADD PARTITION(dt='20220803');
-
